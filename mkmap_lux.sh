eadb0634c5e03774aedbba863df1da6beb5b09fb
Rugosh <tobias.s.kuhn@gmail.com>
2015-02-22 17:06:57 +0100

    changing backend configuration to be able to generate saar and lux node information

---
+++ mkmap_lux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+DEST=$1
+
+
+[ "$DEST" ] || exit 1
+
+cd "$(dirname "$0")"
+
+# ./bat2nodes.py -A -a aliases.json -a aliases_hl.json -a aliases_moelln.json -d $DEST
+sudo ./bat2nodes.py -A -a aliases_lux.json -d $DEST
