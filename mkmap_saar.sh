e97f11b6ecf03acc359422560ac8269a029d1247
Rugosh <tobias.s.kuhn@gmail.com>
2015-06-07 13:53:48 +0200

    extend the traffic.json to the map make

--- mkmap_saar.sh
+++ mkmap_saar.sh
@@ -11,3 +11,5 @@ cd "$(dirname "$0")"
 
 # ./bat2nodes.py -A -a aliases.json -a aliases_hl.json -a aliases_moelln.json -d $DEST
 sudo ./bat2nodes.py -A -a aliases_saar.json -d $DEST
+
+sudo alfred-json -z -r 165 > $DEST"traffic.json"
